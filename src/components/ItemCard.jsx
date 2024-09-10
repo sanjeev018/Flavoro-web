eeab4b4a3772febf4be8d4b8bca07fb4d93ef2cb
sanjeev <sanjeevgour729@gmail.com>
2024-09-10 23:20:36 +0530

    update code

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ItemCard = () => {
+  return (
+    <div>
+      <img src="https://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328~exp=1693062928~hmac=53fd9ad4965
+      80db41c6ca8066510cd89c6b0a0389de8bb6b875a78a1eda09cb5"
+      alt=""
+      className='w-[50px]'
+      />
+    </div>
+  )
+}
+
+export default ItemCard
